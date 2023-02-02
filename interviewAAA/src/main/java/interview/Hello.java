acc5ab20a31d4f935e3892bf9cb8e5edffe79393
HarryGitHub <zahzahriev@yahoo.com>
2023-02-02 20:00:09 +0200

    test

---
+++ interviewAAA/src/main/java/interview/Hello.java
@@ -0,0 +1,17 @@
+package interview;
+
+public class Hello {
+
+    static  class Person {
+        String name;
+        int age;
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
